90c6b81a41feaad9b99203d12d42e1a8a48738ce
zidanhw <zidann.hawari@gmail.com>
2024-08-23 23:27:08 +0700

    1st commit tugas-ts

---
+++ tugas-ts/src/models/order.ts
@@ -0,0 +1,9 @@
+export interface Order {
+    id: number;
+    userId: number;
+    products: {productId: number, quantity: number}[];
+    totalAmount: number;
+    orderDate: Date;
+    status: "pending" | "shipped" | "delivered"
+}
+
